cdd9dc3c32bfa0dfe91628334d085bcc145ac098
CoraJung <hyun4453@gmail.com>
2021-01-19 19:25:15 -0500

    Update snips_train.sh

--- snips_train.sh
+++ snips_train.sh
@@ -2,12 +2,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=5
-#SBATCH --time=5:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mem-per-cpu=20GB
 #SBATCH --job-name=snips_train
 #SBATCH --mail-type=END
-#SBATCH --mail-user=wh916@nyu.edu
-#SBATCH --output=snips_train_%j.out
+#SBATCH --mail-user=hj1399@nyu.edu
+#SBATCH --output=snips_alexa_replication_%j.out
 #SBATCH --gres=gpu:v100:1
   
 # Refer to https://sites.google.com/a/nyu.edu/nyu-hpc/documentation/prince/batch/submitting-jobs-with-sbatch
