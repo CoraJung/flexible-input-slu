a98f9f0efd1204c998f5b887eff2c3060e8a155d
Wendy Hou <wh916@nyu.edu>
2021-05-21 12:39:42 -0400

    Update ASR_Text_Speech_2_Snips.sh

--- shell_files/ASR_Text_Speech_2_Snips.sh
+++ shell_files/ASR_Text_Speech_2_Snips.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem-per-cpu=10GB
 #SBATCH --job-name=ASR-Text-Speech-2-Snips
 #SBATCH --mail-type=END
-#SBATCH --mail-user=hj1399@nyu.edu
-#SBATCH --output=delete_errorMessage_%j.out
+#SBATCH --mail-user=
+#SBATCH --output=ATS_2_Snips_%j.out
 #SBATCH --gres=gpu:v100:1
   
 # Refer to https://sites.google.com/a/nyu.edu/nyu-hpc/documentation/prince/batch/submitting-jobs-with-sbatch
