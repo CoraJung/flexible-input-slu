6843b5028070a9cd974bcb22dc1658ff279fc09e
Wendy Hou <wh916@nyu.edu>
2021-05-21 12:38:48 -0400

    Update ASR_Text_Speech_1_Snips.sh

--- shell_files/ASR_Text_Speech_1_Snips.sh
+++ shell_files/ASR_Text_Speech_1_Snips.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem-per-cpu=10GB
 #SBATCH --job-name=snips_best
 #SBATCH --mail-type=END
-#SBATCH --mail-user=hj1399@nyu.edu
-#SBATCH --output=delete_errorMessage_%j.out
+#SBATCH --mail-user=
+#SBATCH --output=ATS_1_Snips_%j.out
 #SBATCH --gres=gpu:v100:1
   
 # Refer to https://sites.google.com/a/nyu.edu/nyu-hpc/documentation/prince/batch/submitting-jobs-with-sbatch
