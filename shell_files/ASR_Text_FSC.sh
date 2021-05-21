211585436c2d0b4fbd2afa02df0aca587258d4f6
Wendy Hou <wh916@nyu.edu>
2021-05-21 12:37:28 -0400

    Update and rename fluentBERT_trainboth_ours.sh to ASR_Text_FSC.sh

--- shell_files/ASR_Text_FSC.sh
+++ shell_files/ASR_Text_FSC.sh
@@ -4,10 +4,10 @@
 #SBATCH --cpus-per-task=5
 #SBATCH --time=5:00:00
 #SBATCH --mem-per-cpu=10GB
-#SBATCH --job-name=fluentBERT_trainboth_ours
+#SBATCH --job-name=ASR_Text_FSC
 #SBATCH --mail-type=END
-#SBATCH --mail-user=sjc433@nyu.edu
-#SBATCH --output=fluentBERT_trainboth_ours_all_%j.out
+#SBATCH --mail-user=
+#SBATCH --output=ASR_Text_FSC_%j.out
 #SBATCH --gres=gpu:v100:1
   
 # Refer to https://sites.google.com/a/nyu.edu/nyu-hpc/documentation/prince/batch/submitting-jobs-with-sbatch
@@ -25,8 +25,8 @@ MODEL_DIR=//misc/vlgscratch5/PichenyGroup/s2i-common/alexa-slu/best_chkpt/fluent
 # Execute the script
 echo "Model 3 - Finetuning BERT - Train on both GT+ASR - Our config - Test on GT"
 python train.py --data-path=$DATA_PATH/gtasr_gt \
---model-dir=$MODEL_DIR/bert_trainboth_ours --finetune-bert > fluentBERT_trainboth_testraw.out
+--model-dir=$MODEL_DIR/bert_trainboth_ours --finetune-bert > ASR_Text_FSC_testraw.out
 
 echo "Model 3 - Finetuning BERT - Train on both GT+ASR - Our config - Test on ASR"
 python train.py --data-path=$DATA_PATH/gtasr_asr \
---model-dir=$MODEL_DIR/bert_trainboth_ours --finetune-bert --infer-only > fluentBERT_trainboth_testasr.out
+--model-dir=$MODEL_DIR/bert_trainboth_ours --finetune-bert --infer-only > ASR_Text_FSC_testasr.out
