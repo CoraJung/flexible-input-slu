15c57ee3ba7a42e3d7829877da6c32303b579566
Wendy Hou <wh916@nyu.edu>
2021-05-21 13:10:55 -0400

    change absolute path to {YOUR_PATH}

--- shell_files/ASR_Text_Speech_1_FSC.sh
+++ shell_files/ASR_Text_Speech_1_FSC.sh
@@ -19,9 +19,9 @@ module purge
 # Activate the conda environment
 module load anaconda3
 source activate alexa_env
-DATA_PATH=//misc/vlgscratch5/PichenyGroup/s2i-common/alexa-slu/fluentai_asr/data
+DATA_PATH={YOUR_PATH}/fluentai_asr/data
 DATASET=fsc
-MODEL_DIR=/misc/vlgscratch5/PichenyGroup/s2i-common/alexa-slu/best_chkpt/fluentai/np_bert
+MODEL_DIR={YOUR_PATH}/best_chkpt/fluentai/np_bert
 EXPERIMENT=experiments.experiment_triplet_combinedsystem.ExperimentRunnerTriplet
 
 # Execute the script
